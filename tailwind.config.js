593618fc1b6cfa1e2d652f1f90ed1222c6c5ce50
AftabAhmedFahim <aftabahmed.official004@gmail.com>
2026-01-28 22:27:12 +0600

    Added navigation bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ export default {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                monsta: ['"Monsta Fectro"', 'sans-serif'],
             },
         },
     },
